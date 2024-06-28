5e6896698419910f47a28181c6a96bb0c34e9416
Patrik Majer <patrik.majer.pisek@gmail.com>
2024-06-28 21:22:51 +0200

    add stack

--- Program.cs
+++ Program.cs
@@ -1,53 +1,9 @@
 ﻿using Pulumi;
-using AzureNative = Pulumi.AzureNative;
-using Pulumi.AzureNative.Resources;
-using Pulumi.AzureNative.Storage;
-using Pulumi.AzureNative.Storage.Inputs;
-using System.Collections.Generic;
+
 using System.Linq;
+using System.Threading.Tasks;
 
-return await Pulumi.Deployment.RunAsync(() =>
+class Program
 {
-    // Create an Azure Resource Group
-    // https://www.pulumi.com/registry/packages/azure-native/api-docs/resources/resourcegroup/
-    var resourceGroup = new ResourceGroup("Main", new()
-    {
-        ResourceGroupName = "patrik-majer-playground-rg",
-    });
-
-    var userAssignedIdentity = new AzureNative.ManagedIdentity.UserAssignedIdentity("userAssignedIdentity", new()
-    {
-        ResourceGroupName = resourceGroup.Name,
-        ResourceName = "myIdentity-1",
-        
-    });
-    
-    // Create an Azure resource (Storage Account)
-    // var storageAccount = new StorageAccount("sa", new StorageAccountArgs
-    // {
-    //     ResourceGroupName = resourceGroup.Name,
-    //     Sku = new SkuArgs
-    //     {
-    //         Name = SkuName.Standard_LRS
-    //     },
-    //     Kind = Kind.StorageV2
-    // });
-
-    // var storageAccountKeys = ListStorageAccountKeys.Invoke(new ListStorageAccountKeysInvokeArgs
-    // {
-    //     ResourceGroupName = resourceGroup.Name,
-    //     AccountName = storageAccount.Name
-    // });
-
-    // var primaryStorageKey = storageAccountKeys.Apply(accountKeys =>
-    // {
-    //     var firstKey = accountKeys.Keys[0].Value;
-    //     return Output.CreateSecret(firstKey);
-    // });
-
-    // // Export the primary key of the Storage Account
-    // return new Dictionary<string, object?>
-    // {
-    //     ["primaryStorageKey"] = primaryStorageKey
-    // };
-});
+    static Task<int> Main() => Pulumi.Deployment.RunAsync<MyStack>();
+}
